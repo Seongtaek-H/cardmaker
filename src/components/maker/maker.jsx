038cfd623389f17c8c7082e9b47c90c2ad94f1ee
Seongtaek Hwang <hst1993@hanmail.net>
2022-07-21 02:05:30 +0900

    add router & component structure

---
+++ src/components/maker/maker.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Editor from '../editor/editor';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Preview from '../preview/preview';
+import styles from './maker.module.css';
+
+const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: '',
+    },
+    {
+      id: '2',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: '',
+    },
+    {
+      id: '3',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: '',
+    },
+  ]);
+  const navigate = useNavigate();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate('/');
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor cards={cards} />
+        <Preview cards={cards} />
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
